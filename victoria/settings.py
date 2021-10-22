e558d4b9263df7d484411ccb74524ff192e13b2b
jophex <jophex56@gmail.com>
2021-10-22 07:01:41 -0700

    adding stuffs

--- victoria/settings.py
+++ victoria/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 import os
-ROOT_PATH = os.path.dirname('playground\static\css\style.css')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,12 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(ROOT_PATH, 'static')]
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
--- victoria/urls.py
+++ victoria/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    
+
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
